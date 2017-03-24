2508b60d407aa8cf9ee4ccddeec3c5dfdd2a14b6
aps0047 <aps0047@alu.ubu.es>
2017-03-24 12:08:27 +0100

    Se añaden:          - Botones para filtar busquedas.          - Nuevo botón para nuevos pacientes.          - Se añade archivo JavaScript con funciones básicas. #17

---
+++ www/js/ubunurse.js
@@ -0,0 +1,12 @@
+
+
+function displayNuevo(){
+	document.getElementById("nuevoPaciente").style.display='block';
+}
+
+function calculaBarthel(){ 
+	//alert("HI!")
+	
+    alert($("#opcion_baño").find("input:checked").val());
+   	
+	}
