0216f1563ec473c7419c0d39be39e396eabf5d11
aps0047 <aps0047@alu.ubu.es>
2017-04-06 09:28:48 +0200

    Diseño: se añade seccion de notas adicionales en la página del test. #21 Funcionalidad: se añade función para el calculo del test. #26

--- www/js/ubunurse.js
+++ www/js/ubunurse.js
@@ -5,9 +5,43 @@ function displayNuevo(){
 }
 
 function calculaBarthel(){ 
-	//alert("HI!")
 	
-    alert($("#opcion_baño").find("input:checked").val());
+	var sumaBarthel=0;
+	var opcionesBarthel=["#opcion_alimentacion",
+						 "#opcion_baño",
+						 "#opcion_vestirse",
+						 "#opcion_arreglarse",
+						 "#opcion_deposiciones",
+						 "#opcion_miccion",
+						 "#opcion_retrete",
+						 "#opcion_movilidad",
+						 "#opcion_deambular",
+						 "#opcion_escaleras",
+	];
+	
+	
+	for (opcion = 0; opcion < opcionesBarthel.length; opcion++) { 
+    sumaBarthel+=parseInt($(opcionesBarthel[opcion]).find("input:checked").val());
+}
+   
+	switch(true) {
+    case (sumaBarthel<=20):
+        alert("Dependencia Total: "+sumaBarthel);
+        break;
+	case (25<=sumaBarthel && sumaBarthel<=60):
+        alert("Dependencia Severa: "+sumaBarthel);
+        break;
+	case (65<=sumaBarthel && sumaBarthel<=90):
+        alert("Dependencia Moderada: "+sumaBarthel);
+        break;
+	case (sumaBarthel==95):
+        alert("Dependencia Leve: "+sumaBarthel);
+        break;
+	case (sumaBarthel==100):
+        alert("Dependencia Independiente: "+sumaBarthel);
+        break;
+           
+}
    	
 	}
 function cambiarTema(){
